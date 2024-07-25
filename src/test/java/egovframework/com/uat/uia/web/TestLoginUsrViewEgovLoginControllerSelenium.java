b22f98ea87c96cf6eaa2673934dc79fad719534e
이백행 <dlqorgod@naver.com>
2024-07-25 12:58:54 +0900

    로그인 화면 이동

---
+++ src/test/java/egovframework/com/uat/uia/web/TestLoginUsrViewEgovLoginControllerSelenium.java
@@ -0,0 +1,23 @@
+package egovframework.com.uat.uia.web;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestLoginUsrViewEgovLoginControllerSelenium {
+
+	WebDriver driver;
+
+	@Before
+	public void setup() {
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	public void test() {
+		// 로그인 화면 이동
+		driver.get("http://localhost:8080/egovframework-all-in-one/uat/uia/egovLoginUsr.do");
+	}
+
+}
